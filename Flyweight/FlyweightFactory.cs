5caa6856a600d07a8cce08cfb2292d0255a2b195
Roman Karpov <poma.05@mail.ru>
2020-02-06 18:21:01 +0400

    init

---
+++ Flyweight/FlyweightFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    public class FlyweightFactory
+    {
+        private Hashtable flyweights = new Hashtable();
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+        public Flyweight GetFlyweight(string key)
+        {
+            return ((Flyweight)flyweights[key]);
+        }
+    }
+}
---
+++ FlyweightRealWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FlyweightRealWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string document = "AAZZBBZB";
+            char[] chars = document.ToCharArray();
+            CharacterFactory factory = new CharacterFactory();
+            int pointSize = 10;
+            foreach (char c in chars)
+            {
+                pointSize++;
+                Character character = factory.GetCharacter(c);
+                character.Display(pointSize);
+            }
+            Console.ReadKey();
+        }
+    }
+}
